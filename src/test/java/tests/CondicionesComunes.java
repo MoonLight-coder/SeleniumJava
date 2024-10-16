5ae97170cf59019640dd9fb80d898c83c94a407c
MoonLight-coder <angelogonzacorre111@gmail.com>
2024-10-16 17:30:33 -0300

    "inalización del curso de Selenium"

---
+++ src/test/java/tests/CondicionesComunes.java
@@ -0,0 +1,40 @@
+package tests;
+
+import helpers.Screenshooter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.ArrayList;
+
+public class Precondiciones {
+    private WebDriver driver;
+    ArrayList<String> tabs;
+    @BeforeMethod
+    public void setUp() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+//        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--headless");
+//        driver = new ChromeDriver(chromeOptions);
+        driver.navigate().to("https://practicetestautomation.com/practice-test-login/");
+//        JavascriptExecutor javascriptExecutor = (JavascriptExecutor)driver;
+//        String googleWindow = "window.open('https://www.google.com')";
+//        javascriptExecutor.executeScript(googleWindow);
+//        tabs = new ArrayList<String> (driver.getWindowHandles());
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        if(!result.isSuccess()){
+            Screenshooter.takeScreenshot("Error", driver);
+        }
+        driver.switchTo().window(tabs.get(1)).close();
+        driver.switchTo().window(tabs.get(0)).close();
+    }
+
+}
